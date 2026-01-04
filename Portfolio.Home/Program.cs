e150d72ea93f1b18eb6e2793c737993396df761f
Toni Borozan <tborozan@hotmail.com>
2026-01-04 13:06:49 +0100

    migrate to blazor wasm

--- Portfolio.Home/Program.cs
+++ Portfolio.Home/Program.cs
@@ -1,16 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Portfolio.Home;
 
-builder.Services.AddRazorPages();
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var app = builder.Build();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages().WithStaticAssets();
-
-await app.RunAsync();
+await builder.Build().RunAsync();
